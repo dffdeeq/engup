048417c3878fca62989a4f6a6b40ec779149dde4
dffdeeq <frodin.frodo5@mail.ru>
2024-06-20 18:06:00 +0300

    rabbitmq producer/consumer update

--- src/rabbitmq/producer/factories/apihost.py
+++ src/rabbitmq/producer/factories/apihost.py
@@ -1,19 +1,28 @@
 import json
+import os.path
 import typing as T  # noqa
 
 from aio_pika import Message
 
 from src.libs.adapter import Adapter
 from src.rabbitmq.producer.factory import RabbitMQProducerFactory
+from src.settings.static import TEMP_FILES_DIR
 
 
 class ApiHostProducer(RabbitMQProducerFactory):
-    def __init__(self, dsn_string: str, exchange_name: str, adapter: Adapter):
-        super().__init__(dsn_string, exchange_name, adapter)
+    def __init__(self, dsn_string: str, adapter: Adapter, exchange_name: str = 'direct'):
+        super().__init__(dsn_string=dsn_string, adapter=adapter, exchange_name=exchange_name)
 
-    async def send_to_transcription(self, filepaths: T.List[str]):
+    async def create_task_send_to_transcription(self, filenames: T.List[str]):
         message = Message(
-            body=bytes(json.dumps({'files': filepaths}), 'utf-8'),
+            body=bytes(json.dumps({'files': [os.path.join(TEMP_FILES_DIR, f) for f in filenames]}), 'utf-8'),
             content_type='json',
         )
-        await self._publish(message, 'to_transcription')
+        await self._publish(message, 'apihost_to_transcription')
+
+    async def create_task_update_answers(self, answers_and_filenames: T.List[T.Dict[str, str]]):
+        message = Message(
+            body=bytes(json.dumps(answers_and_filenames), 'utf-8'),
+            content_type='json',
+        )
+        await self._publish(message, 'apihost_update_answers')
