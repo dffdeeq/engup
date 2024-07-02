724da1e250e0366acb152490eef2d08c51bd7c19
dffdeeq <frodin.frodo5@mail.ru>
2024-07-02 20:37:50 +0300

    order of essay review has been changed

--- src/bot/core/filters/essay_filter.py
+++ src/bot/core/filters/essay_filter.py
@@ -12,10 +12,10 @@ class EssayFilter(Filter):
     async def __call__(self, message: Message) -> bool:
         if is_copypaste(message.text):
             warning = DefaultMessages.TEXT_IS_COPY_PASTE
-        elif not is_english(message.text):
-            warning = DefaultMessages.TEXT_IS_NOT_ENGLISH
         elif len(message.text.split()) < 150:
             warning = DefaultMessages.TOO_SHORT_TEXT_WARNING
+        elif not is_english(message.text):
+            warning = DefaultMessages.TEXT_IS_NOT_ENGLISH
         else:
             return True
 
