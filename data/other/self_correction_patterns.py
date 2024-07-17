96dc545d73a9b54e0f0ed834135ee7f1763ce6a3
dffdeeq <frodin.frodo5@mail.ru>
2024-07-17 20:00:37 +0300

    fluency_coherence logic

---
+++ data/other/self_correction_patterns.py
@@ -0,0 +1,16 @@
+SELF_CORRECTION_PATTERNS = [
+    'i mean', 'sorry', 'actually', 'let me rephrase', 'what I meant to say',
+    'let me think', 'in other words', 'I should say', 'that\'s not quite right',
+    'to put it another way', 'or rather', 'let me correct myself', 'how can I put this',
+    'I meant', 'I guess I should say', 'wait a moment', 'let me clarify', 'let me start over',
+    'sorry, that\'s not correct', 'let me rephrase that', 'I need to correct that',
+    'I should clarify', 'I apologize, that\'s wrong', 'that\'s not what I meant',
+    'to be more precise', 'actually, it\'s better to say', 'that\'s incorrect, what I meant is',
+    'let\'s put it differently', 'let me correct that', 'I should rephrase',
+    'excuse me, I need to correct that', 'what I was trying to say is', 'I should have said',
+    'not that, but', 'on second thought', 'I misspoke', 'I need to amend that', 'I need to revise that',
+    'what I intended to say', 'I think a better way to say it is', 'I need to fix that',
+    'let me put it another way', 'let me change that', 'that wasn\'t right, I meant',
+    'my apologies, that\'s wrong', 'let me restate that', 'I should modify that',
+    'to be clearer', 'allow me to rephrase'
+]
