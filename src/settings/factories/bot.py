b240e8c3194aa09a2937f85ba8dbbca90739ecc0
dffdeeq <frodin.frodo5@mail.ru>
2024-07-18 19:03:29 +0300

    new field (admin_ids) for bot settings

--- src/settings/factories/bot.py
+++ src/settings/factories/bot.py
@@ -7,9 +7,11 @@ from src.settings.factory import SettingsFactory
 
 class BotSettings(SettingsFactory):
     bot_token: str = Field(description="Telegram bot token")
+    admin_ids: list[str] = Field(description="List of admins IDs")
 
     @classmethod
     def from_dict(cls, settings_dict: T.Dict[str, str]) -> 'BotSettings':
         return BotSettings(
             bot_token=settings_dict.get('BOT_SETTINGS_BOT_TOKEN'),
+            admin_ids=settings_dict.get('BOT_SETTINGS_ADMIN_IDS').split(','),
         )
