a5106922b6188429b640b78fbca5dd4df25b0460
dffdeeq <frodin.frodo5@mail.ru>
2024-08-09 18:21:40 +0300

    TgUserReview model and migration

--- src/postgres/models/poll_feedback.py
+++ src/postgres/models/poll_feedback.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, Column, JSON, ForeignKey, BigInteger, DateTime, func
+from sqlalchemy import Integer, Column, JSON, ForeignKey, BigInteger, DateTime, func, Text
 
 from src.postgres.base import Base
 
@@ -10,4 +10,15 @@ class PollFeedback(Base):
     user_id = Column(BigInteger, ForeignKey('tg_user.id'))
     feedback_json = Column(JSON)
 
+    created_at = Column(DateTime(timezone=True), default=func.now())
+
+
+class TgUserReview(Base):
+    __tablename__ = 'tg_user_review'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(BigInteger, ForeignKey('tg_user.id'))
+
+    rating = Column(Integer)
+    text = Column(Text)
     created_at = Column(DateTime(timezone=True), default=func.now())
