c6d09ebe0eeb7e079e74c0d556ec1c3e15ce84e5
dffdeeq <frodin.frodo5@mail.ru>
2024-08-14 18:53:59 +0300

    implementation of sending utm tags

--- src/bot/handlers/command_handler/start_command.py
+++ src/bot/handlers/command_handler/start_command.py
@@ -1,9 +1,12 @@
+import uuid
+
 from aiogram import Router
 from aiogram.filters import CommandStart, CommandObject
 from aiogram.types import Message
 
 from src.bot.handlers.defaults.menu_default import answer_menu
 from src.bot.injector import INJECTOR
+from src.libs.factories.analytics.models.event_data import EventData
 from src.services.factories.tg_user import TgUserService
 
 router = Router(name=__name__)
@@ -12,12 +15,41 @@ router = Router(name=__name__)
 @router.message(CommandStart(), INJECTOR.inject_tg_user)
 async def command_start_handler(message: Message, command: CommandObject, tg_user_service: TgUserService) -> None:
     user_referrer_id = None
-    referrer_id = command.args
-    if referrer_id:
-        referral = await tg_user_service.repo.get_tg_user_by_tg_id(int(referrer_id))
-        if referral:
-            user_referrer_id = referral.id
+    command_args = command.args
+    utm_source = None
+    utm_medium = None
+    utm_campaign = None
+    utm_content = None
+    if command_args:
+        if '__' in command_args:
+            args = command_args.split('__')
+            variables = {}
+            for item in args:
+                key, value = item.split("=")
+                variables[key] = value
 
-    await tg_user_service.get_or_create_tg_user(message.from_user.id, message.from_user.username, user_referrer_id)
+                utm_source = variables.get('utm_source', 'None')
+                utm_medium = variables.get('utm_medium', 'None')
+                utm_campaign = variables.get('utm_campaign', 'None')
+                utm_content = variables.get('utm_content', 'None')
+
+            await tg_user_service.adapter.analytics_client.send_event(
+                str(uuid.uuid4()),
+                EventData(
+                    utm_source=utm_source,
+                    utm_medium=utm_medium,
+                    utm_campaign=utm_campaign,
+                    utm_content=utm_content,
+                    event_name='conversion_event_signup',
+                )
+            )
+
+        else:
+            referral = await tg_user_service.repo.get_tg_user_by_tg_id(int(command_args))
+            if referral:
+                user_referrer_id = referral.id
+
+    await tg_user_service.get_or_create_tg_user(message.from_user.id, message.from_user.username, user_referrer_id,
+                                                utm_source, utm_medium, utm_campaign, utm_content)
     await tg_user_service.mark_user_activity(message.from_user.id, 'go to menu')
     await answer_menu(message)
--- src/bot/handlers/message_handler/pricing.py
+++ src/bot/handlers/message_handler/pricing.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import uuid
 
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
@@ -9,6 +10,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 from src.bot.handlers.defaults.menu_default import answer_menu
 from src.bot.handlers.defaults.pricing_default import answer_pricing
 from src.bot.injector import INJECTOR
+from src.libs.factories.analytics.models.event_data import EventData
 from src.services.factories.tg_user import TgUserService
 
 router = Router(name=__name__)
@@ -28,12 +30,12 @@ async def pricing_callback(callback: types.CallbackQuery, tg_user_service: TgUse
     await answer_pricing(callback, user)
 
 
-@router.callback_query(F.data.startswith('buy_pts_by_tg_stars'))
-async def buy_pts_by_tg_start(callback: types.CallbackQuery):
+@router.callback_query(F.data.startswith('buy_pts_by_tg_stars'), INJECTOR.inject_tg_user)
+async def buy_pts_by_tg_start(callback: types.CallbackQuery, tg_user_service: TgUserService):
     await callback.answer()
 
     price = {
-        3: [LabeledPrice(label="3 PTs", amount=150)],
+        3: [LabeledPrice(label="3 PTs", amount=1)],
         10: [LabeledPrice(label="10 PTs", amount=450)],
         100: [LabeledPrice(label="100 PTs", amount=4000)]
     }
@@ -55,6 +57,18 @@ async def buy_pts_by_tg_start(callback: types.CallbackQuery):
         }),
         currency='XTR',
         prices=price[amount]
+    )
+
+    user = await tg_user_service.get_or_create_tg_user(callback.from_user.id)
+    await tg_user_service.adapter.analytics_client.send_event(
+        str(uuid.uuid4()),
+        EventData(
+            utm_source=user.utm_source,
+            utm_medium=user.utm_medium,
+            utm_campaign=user.utm_campaign,
+            utm_content=user.utm_content,
+            event_name='conversion_event_begin_checkout',
+        )
     )
 
 
@@ -81,6 +95,17 @@ async def successful_payment_handler(message: types.Message, state: FSMContext, 
             await tg_user_service.mark_user_activity(message.from_user.id, 'buy points')
             await tg_user_service.mark_user_balance(message.from_user.id, 'Telegram', price, 'TGS')
             await tg_user_service.mark_user_pts(message.from_user.id, 'buy', purchased_pts)
+
+            await tg_user_service.adapter.analytics_client.send_event(
+                str(uuid.uuid4()),
+                EventData(
+                    utm_source=user_instance.utm_source,
+                    utm_medium=user_instance.utm_medium,
+                    utm_campaign=user_instance.utm_campaign,
+                    utm_content=user_instance.utm_content,
+                    event_name='conversion_event_purchase',
+                )
+            )
     else:
         logging.critical(f'Payment failed for user {message.from_user.id} (from payload - {user_id_from_payload}) '
                          f'(user_id_from_payload != message.from_user.id)')
