157a06b823f876a473c4a9105b7ec8668017bbbe
dffdeeq <frodin.frodo5@mail.ru>
2024-08-14 18:55:23 +0300

    excess code removed

--- src/libs/factories/analytics/mixins/send_event.py
+++ src/libs/factories/analytics/mixins/send_event.py
@@ -1,11 +1,7 @@
-import asyncio
 import logging
-import uuid
 
 from src.libs.factories.analytics.base import BaseAnalyticsClient
 from src.libs.factories.analytics.models.event_data import EventData
-from src.libs.http_client import HttpClient
-from src.settings import Settings
 
 
 class SendEventMixin(BaseAnalyticsClient):
@@ -35,21 +31,3 @@ class SendEventMixin(BaseAnalyticsClient):
             return
         else:
             logging.error('Failed sending event to Google Analytics')
-
-
-async def main(client: SendEventMixin):
-    client_id = str(uuid.uuid4())
-    await client.send_event(client_id, EventData(
-        utm_source='test1',
-        utm_medium='test2',
-        utm_campaign='test3',
-        utm_content='test4',
-        event_name='conversion_event_begin_checkout',
-    ))
-
-
-if __name__ == '__main__':
-    settings = Settings.new()
-    http_client = HttpClient()
-    client = SendEventMixin(http_client, settings.analytics)
-    asyncio.run(main(client))
