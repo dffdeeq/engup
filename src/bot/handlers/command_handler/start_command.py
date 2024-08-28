e522735ac600bc1442fedddad89f3c993d4082bf
dffdeeq <frodin.frodo5@mail.ru>
2024-08-28 20:17:52 +0300

    metrics logic changed

--- src/bot/handlers/command_handler/start_command.py
+++ src/bot/handlers/command_handler/start_command.py
@@ -27,54 +27,41 @@ async def command_start_handler(
 ) -> None:
     user_referrer_id = None
     command_args = command.args
-    utm_source = None
-    utm_medium = None
-    utm_campaign = None
-    utm_content = None
-    utm_term = None
-    gad_source = None
-    gclid = None
+    umt_data_dict = {}
+
     if command_args:
-        try:
+        user = await tg_user_service.repo.get_tg_user_by_tg_id(message.from_user.id)
+        if not user:
             try:
-                metrics_uuid = uuid.UUID(command_args)
-                metrics_data = await metrics_service.get_metrics_data(metrics_uuid)
-                metrics_string = metrics_data.metrics_string.split('__')
-                logging.info(f'args: {metrics_string}')
-                variables = {}
-                for item in metrics_string:
-                    key, value = item.split("=")
-                    variables[key] = value
+                try:
+                    metrics_uuid = uuid.UUID(command_args)
+                    metrics_data = await metrics_service.get_metrics_data(metrics_uuid)
+                    metrics_string = metrics_data.metrics_string.split('__')
+                    logging.info(f'args: {metrics_string}')
+                    for item in metrics_string:
+                        key, value = item.split("=")
+                        umt_data_dict[key] = value
 
-                utm_source = variables.get('utm_source', 'None')
-                utm_medium = variables.get('utm_medium', 'None')
-                utm_campaign = variables.get('utm_campaign', 'None')
-                utm_content = variables.get('utm_content', 'None')
-                utm_term = variables.get('utm_term', 'None')
-                gad_source = variables.get('gad_source', 'None')
-                gclid = variables.get('gclid', 'None')
-
-                await tg_user_service.adapter.analytics_client.send_event(
-                    str(uuid.uuid4()),
-                    EventData(
-                        utm_source=utm_source,
-                        utm_medium=utm_medium,
-                        utm_campaign=utm_campaign,
-                        utm_content=utm_content,
-                        event_name='conversion_event_signup',
+                    await tg_user_service.adapter.analytics_client.send_event(
+                        str(uuid.uuid4()),
+                        EventData(
+                            utm_source=umt_data_dict.get('utm_source', None),
+                            utm_medium=umt_data_dict.get('utm_medium', None),
+                            utm_campaign=umt_data_dict.get('utm_campaign', None),
+                            utm_content=umt_data_dict.get('utm_content', None),
+                            event_name='conversion_event_signup',
+                        )
                     )
-                )
 
-            except ValueError:
-                referral = await tg_user_service.repo.get_tg_user_by_tg_id(int(command_args))
-                if referral:
-                    user_referrer_id = referral.id
-        except Exception as e:
-            logging.error(e)
+                except ValueError:
+                    referral = await tg_user_service.repo.get_tg_user_by_tg_id(int(command_args))
+                    if referral:
+                        user_referrer_id = referral.id
+            except Exception as e:
+                logging.error(e)
 
     await tg_user_service.get_or_create_tg_user(
-        message.from_user.id, message.from_user.username, user_referrer_id, utm_source, utm_medium, utm_campaign,
-        utm_content, utm_term, gad_source, gclid
+        message.from_user.id, message.from_user.username, user_referrer_id, umt_data_dict
     )
     await tg_user_service.mark_user_activity(message.from_user.id, 'go to menu')
     await answer_menu(message)
