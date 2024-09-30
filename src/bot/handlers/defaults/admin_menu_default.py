b30bbdc87c542e6694d50d7caed7a53426180255
dffdeeq <frodin.frodo5@mail.ru>
2024-09-30 19:34:07 +0300

    admin menu mini app

--- src/bot/handlers/defaults/admin_menu_default.py
+++ src/bot/handlers/defaults/admin_menu_default.py
@@ -1,22 +1,26 @@
 import typing as T  # noqa
 
-from aiogram.types import InlineKeyboardButton, Message, CallbackQuery
+from aiogram.types import InlineKeyboardButton, Message, CallbackQuery, WebAppInfo
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-async def get_admin_menu() -> T.Tuple[str, InlineKeyboardBuilder]:
+async def get_admin_menu(mini_app_pronunciation_url: str) -> T.Tuple[str, InlineKeyboardBuilder]:
     text = 'Admin Menu\n----------'
     builder = InlineKeyboardBuilder([
         [
             InlineKeyboardButton(text='Run task', callback_data='admin_run_task_processing default'),
             InlineKeyboardButton(text='Run task premium', callback_data='admin_run_task_processing premium'),
+            InlineKeyboardButton(
+                text="Open Mini App",
+                web_app=WebAppInfo(url=mini_app_pronunciation_url)
+            )
         ],
     ])
     return text, builder
 
 
-async def answer_admin_menu(instance: T.Union[Message, CallbackQuery]) -> None:
-    text, builder = await get_admin_menu()
+async def answer_admin_menu(instance: T.Union[Message, CallbackQuery], mini_app_pronunciation_url: str) -> None:
+    text, builder = await get_admin_menu(mini_app_pronunciation_url)
     if isinstance(instance, CallbackQuery):
         await instance.message.edit_text(text, reply_markup=builder.as_markup())
     else:
